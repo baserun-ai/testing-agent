a8a20e9636377dea6882bad850ea5dbf5acae893
Erik Peterson <e@eriklp.com>
2023-09-20 10:15:32 -0700

    Updates with Anthropic support

--- demo/main.py
+++ demo/main.py
@@ -3,9 +3,11 @@ import logging
 import sys
 
 import baserun as baserun
+from baserun import Baserun
 from dotenv import load_dotenv
 from langchain.agents import initialize_agent, AgentType, load_tools
-from langchain.chat_models import ChatOpenAI
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.chat_models import ChatOpenAI, ChatAnthropic
 
 load_dotenv()
 
@@ -13,15 +15,34 @@ logger = logging.getLogger(__name__)
 logging.basicConfig()
 logger.setLevel(logging.DEBUG)
 
+logger = logging.getLogger(__name__)
+
+
+def choose_llm(provider: str, use_streaming: bool = False):
+    if provider == "openai":
+        if use_streaming:
+            return ChatOpenAI(
+                temperature=0,
+                model_name="gpt-3.5-turbo-16k-0613",
+                streaming=True,
+                callbacks=[StreamingStdOutCallbackHandler()],
+            )
+        else:
+            return ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613")
+    if provider == "anthropic":
+        return ChatAnthropic(streaming=use_streaming)
+
+    raise NotImplementedError
+
 
 @baserun.trace
-def main(user_input=""):
+def main(provider="openai", user_input="", use_streaming=False):
     if not user_input:
         print("What would you like me to do?")
         print("> ", end="")
         user_input = input()
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613")
+    llm = choose_llm(provider, use_streaming)
 
     tools = load_tools(["serpapi", "llm-math", "wikipedia"], llm=llm)
 
@@ -32,8 +53,8 @@ def main(user_input=""):
 
 
 if __name__ == "__main__":
-    baserun.init()
+    Baserun.init()
     if sys.argv[-1] not in __file__:
-        main(sys.argv[-1])
+        main(user_input=sys.argv[-1])
     else:
         main()
